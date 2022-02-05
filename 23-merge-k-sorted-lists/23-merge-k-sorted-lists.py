7f9c8f568913f1dd48b2c8a2e310b104352bc878
AnhHChu <13889581+anhhchu@users.noreply.github.com>
2022-02-05 13:27:19 -0600

    Time: 116 ms (72.36%), Space: 18.1 MB (37.41%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -6,24 +6,37 @@
 
 import heapq
 class Solution:
+    # compare k elements at each index of all lists using minHeap # O(k)
+    # pop from minHeap O(logk)
+    
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        # put all nums into a min heap
-        # build an output list by popping element from min heap
-        # O(NlogN) = (500*10^4)*log(500*10^4)
-        minHeap = [] # O(N)
-        for head in lists: #O(k)
-            node = head
-            while node: # O(500)
-                heapq.heappush(minHeap, node.val) # O(logN)
-                node = node.next
-                
-        #print(minHeap)
-        head = ListNode(0)
-        node = head
+        # create empty output linkedList with sentinel head node
+        head = node = ListNode(0) 
+        minHeap = []
+        count = 0
+        for lst in lists: # add first k elements into heap # O(k)
+            count += 1
+            if lst:
+                # add  `lst.val` and `count` for comparison
+                heapq.heappush(minHeap, (lst.val, count, lst)) 
         
         while minHeap: # O(N)
-            node.next = ListNode(heappop(minHeap))
-            node = node.next 
+            count += 1
+            val, _, current = heapq.heappop(minHeap) # O(logk)
+            # add the current node to output list so we don't have to create a new node
+            node.next = current 
+            # add the next node to minHeap for the next iteration
+            if current.next:
+                heapq.heappush(minHeap, (current.next.val, count, current.next)) # O(logk)
+            # remove the next pointer for current node
+            current.next = None
+            # update node 
+            node = node.next
             
         return head.next
+            
+            
+            
+        
+                
         
