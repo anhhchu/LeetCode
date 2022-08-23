6947b113647ff9beffaf83d66f2284882396b862
Chipmunk❤️DEV <13889581+anhhchu@users.noreply.github.com>
2022-08-22 21:41:05 -0700

    Time: 112 ms (90.68%), Space: 19.8 MB (6.30%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,8 +1,24 @@
-from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # O(N)
-        counter = Counter(nums)
-        # O(NlogN)
-        sortedCounter = [k for k, v in sorted(counter.items(), key = lambda item : -item[1] )]
-        return sortedCounter[:k]
+        count = {} 
+        freq_bucket = [ [] for i in range(len(nums) + 1) ]
+        
+        # populate count
+        # count freq the old-fashioned way
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+            
+        for num, c in count.items():
+            freq_bucket[c].append(num)
+           
+        # get result from freq_bucket
+        res = []
+        # loop reversedly
+        for i in range(len(freq_bucket) - 1, 0 , -1): 
+            for num in freq_bucket[i]: # empty or not
+                res.append(num)
+                if len(res) == k:
+                    return res
+                
+            
+        
