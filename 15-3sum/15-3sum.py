31ea5a4a6bc0cedb271e6572562dbfb475bc5cc9
Chipmunk❤️DEV <13889581+anhhchu@users.noreply.github.com>
2022-09-04 06:25:19 -0700

    Time: 1538 ms (41.88%), Space: 18.1 MB (39.41%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,33 +1,31 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         """
-        -2 0 0 0 0 0 2 3
+        -2 0 0 0 0 0 1 1 2 3
         """
         # first sort the list so we can call 2 sum
         nums.sort()
         output = []
         
-        def twoSum(l, r, target):
-            start = l 
-            while l < r:
-                if nums[l] + nums[r] == target: 
-                    if l == start or (l > start and nums[l-1] != nums[l]): 
-                        output.append([-target, nums[l], nums[r]])
-                        r -= 1
-                    l += 1
-                elif nums[l] + nums[r] > target:
-                    r -= 1
-                elif nums[l] + nums[r] < target:
-                    l += 1 
-            return
-        
         
         for i, num in enumerate(nums):
-            # call 2sum on the remaining list to find val1 + val2 = -num
+            # binary search in list nums[i+1:]
             # skip duplicate value
             if i == 0 or (i > 0 and nums[i-1] != num): 
-                twoSum(i + 1, len(nums) - 1, -num)
-        
+                l, r = i+1, len(nums) - 1
+                while l < r:
+                    if nums[l] + nums[r] < -nums[i]:
+                        l += 1
+                    elif nums[l] + nums[r] > -nums[i]:
+                        r -= 1
+                    elif nums[l] + nums[r] == -nums[i]:
+                        # add to output
+                        output.append([nums[i], nums[l], nums[r]])
+                        l += 1
+                        # skip duplicate value
+                        while nums[l] == nums[l-1] and l < r:
+                            l += 1
+                        r -= 1
         return output
             
                     
