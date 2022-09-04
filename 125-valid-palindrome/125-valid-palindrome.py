c95b4e516a3957d4e6b7683a359740dd943f00e6
Chipmunk❤️DEV <13889581+anhhchu@users.noreply.github.com>
2022-09-03 22:55:10 -0700

    Time: 105 ms (17.73%), Space: 14.4 MB (85.78%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,5 +1,20 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        new = ''.join([char.lower() for char in s if char.isalnum()]) # O(N)
-        return new == new[::-1]
         
+        # 2 pointers, check value of s[L], s[R] -> inc L, dec R
+        # encounter the non-alpha char, change the pointer
+        # L >= R -> return True
+        # s[L] != s[R] val is alphanum -> return False and exit loop
+        
+        L, R = 0, len(s)-1
+        while L < R:
+            while L < R and (not s[L].isalnum()):
+                L += 1
+            while L < R and (not s[R].isalnum()):
+                R -= 1
+            if s[L].lower() != s[R].lower():
+                return False
+            L += 1
+            R -= 1
+            
+        return True
